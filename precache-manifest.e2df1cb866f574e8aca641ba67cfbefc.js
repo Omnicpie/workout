a14e2e5079d5195fb15dd695cf15be3e984afbbf
omnicpie <ryananderson1999@hotmail.co.uk>
2020-11-23 13:27:19 +0000

    Updates

---
+++ precache-manifest.e2df1cb866f574e8aca641ba67cfbefc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fd10854ca766801aee1bf1c7f5d28e9",
+    "url": "/workout/index.html"
+  },
+  {
+    "revision": "a019258a72d9a6759540",
+    "url": "/workout/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "c4df5785942163799e1a",
+    "url": "/workout/static/css/main.afbac168.chunk.css"
+  },
+  {
+    "revision": "a019258a72d9a6759540",
+    "url": "/workout/static/js/2.915ca4f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/workout/static/js/2.915ca4f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4df5785942163799e1a",
+    "url": "/workout/static/js/main.b657909e.chunk.js"
+  },
+  {
+    "revision": "b44d9f4e455aadda26d5",
+    "url": "/workout/static/js/runtime-main.e5bacfdb.js"
+  }
+]);
